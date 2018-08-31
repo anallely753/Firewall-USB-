406e6e0b0ee3d857142feec14483ce206464d862
anallely753 <anallely.0710@gmail.com>
2018-08-31 04:41:58 -0500

    Parte 1 del switch

--- pfirewall.sh
+++ pfirewall.sh
@@ -15,5 +15,15 @@ elif [ $( egrep --count $usb white) -ne 0];
 then
 zenity --info --text "Tu USB esta en la lista blanca\nSe montara para usarse"
 mount /dev/sdb /media/usb
+else 
+opc=$(zenity --entry --text "**Firewall USB**\n1)Poner USB en la lista blanca\n2)Poner USB en la lista negra" --entry-text "Opcion")
+
+case $opc in
+(1)
+zenity --info --text "Tu USB se ha agregado con exito a la lista blanca"
+echo "$usb" >> white
+mount /dev/sdb /media/usb
+exit
+;;
 
 
