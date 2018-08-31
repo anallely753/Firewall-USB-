62970bfbdc1e009f4901eb4392c5cf79dc7fc9ed
Mario <marioalverto543@hotmail.com>
2018-08-31 04:10:36 -0500

    Busqueda de la USB

--- pfirewall.sh
+++ pfirewall.sh
@@ -7,4 +7,13 @@ usb=$(dmesg|tail -n 15 | egrep Serial | awk '{print $5'})
 
 umount /run/MULTIBOOT 
 
+if [ $( egrep --count $usb black) -ne 0];
+then
+zenity --info --text "Tu USB no se puede ejecutar"
+
+elif [ $( egrep --count $usb white) -ne 0];
+then
+zenity --info --text "Tu USB esta en la lista blanca\nSe montara para usarse"
+mount /dev/sdb /media/usb
+
 
